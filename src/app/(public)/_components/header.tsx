ca4a625212b0a8bc38a4076443b4408e877490f5
gustavo <gustavols2100@gmail.com>
2025-10-14 22:00:28 -0300

    Feat: cria header e implementa sheet do Shadcn

---
+++ src/app/(public)/_components/header.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+
+export function Header() {
+  return (
+    <header className="fixed top-0 right-0 left-0 z-[999] py-4 px-6 bg-white">
+      <div className="container mx-auto flex items-center justify-between ">
+        <Link href="/" className="text-zinc-900 font-bold text-3xl">
+          Odonto
+          <span className="text-emerald-500 ">Pro</span>
+        </Link>
+
+        <nav>
+          <a href="#" className="hidden md:flex">
+            Profissionais
+          </a>
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild className="md:hidden">
+            <Button
+              className="text-black hover:bg-transparent"
+              variant="ghost"
+              size="icon"
+            >
+              <Menu className="w-6 h-6" />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent
+            side="right"
+            className="w-[240px] sm:w-[300px] z-[9999]"
+          >
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription>Veja nossos links</SheetDescription>
+            <SheetHeader></SheetHeader>
+
+            <nav>
+              <a href="#" className="hidden md:flex">
+                Profissionais
+              </a>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
+  );
+}
