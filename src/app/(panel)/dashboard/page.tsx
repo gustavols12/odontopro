3aeb30197ab70d483ea6a700959fa74644f6e1ea
gustavols12 <gustavols2100@gmail.com>
2025-10-16 16:44:03 -0300

    Feat: Priva rotas para apenas usuários logados

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,4 +1,10 @@
-export default function Dashboard() {
+import getSession from '@/lib/getSession';
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session) redirect('/');
   return (
     <div>
       <h1>página dashboard</h1>
--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -11,10 +11,16 @@ import {
 import { Button } from '@/components/ui/button';
 import { LogIn, Menu } from 'lucide-react';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { handleRegister } from '../_actions/login';
 
 export function Header() {
   const navItems = [{ href: '#profissionais', label: 'Profissionais' }];
-  const session = null;
+  const { data: session, status } = useSession();
+
+  async function handleLogin() {
+    await handleRegister('github');
+  }
 
   const NavLinks = () => (
     <>
@@ -31,15 +37,17 @@ export function Header() {
         </Button>
       ))}
 
-      {session ? (
+      {status === 'loading' ? (
+        <></>
+      ) : session ? (
         <Link
           href="/dashboard"
-          className="flex items-center justify-center gap-2"
+          className="flex items-center justify-center gap-2 bg-zinc-900 text-white py-1 rounded-md px-4"
         >
           Acessar clinica
         </Link>
       ) : (
-        <Button>
+        <Button onClick={handleLogin}>
           <LogIn />
           Portal da clinica
         </Button>
