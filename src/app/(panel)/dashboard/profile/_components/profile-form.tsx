a5aea138a6aca4ce1be9d50039a73f9fadb915af
gustavols12 <gustavols2100@gmail.com>
2025-10-17 17:01:11 -0300

    Feat: Finaliza componente de formulário do perfil

--- src/app/(panel)/dashboard/profile/_components/profile-form.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile-form.tsx
@@ -1,26 +1,26 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
 
 const profileSchema = z.object({
-  name: z.string().min(1, { message: "o nome é obrigatório" }),
+  name: z.string().min(1, { message: 'o nome é obrigatório' }),
   address: z.string().min(1).optional(),
   phone: z.string().min(1).optional(),
   status: z.string(),
-  timeZone: z.string().min(1, { message: "o time zone é obrigatório" }),
+  timeZone: z.string().min(1, { message: 'o time zone é obrigatório' }),
 });
 
-type ProfileFormData = z.infer<typeof profileSchema>;
+export type ProfileFormData = z.infer<typeof profileSchema>;
 
 export function useProfileForm() {
   return useForm<ProfileFormData>({
     resolver: zodResolver(profileSchema),
     defaultValues: {
-      name: "",
-      address: "",
-      phone: "",
-      status: "Ativo",
-      timeZone: "",
+      name: '',
+      address: '',
+      phone: '',
+      status: 'Ativo',
+      timeZone: '',
     },
   });
 }
--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { useProfileForm } from "./profile-form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import imageTeste from "../../../../../../public/foto1.png";
+import { ProfileFormData, useProfileForm } from './profile-form';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import imageTeste from '../../../../../../public/foto1.png';
 import {
   Form,
   FormControl,
@@ -11,24 +11,76 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import { Label } from "@/components/ui/label";
-import Image from "next/image";
+} from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
+import Image from 'next/image';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export function ProfileContent() {
   const form = useProfileForm();
+  const [selectedHours, setSelectedHours] = useState<string[]>([]);
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
+  function generateTimeSlots(): string[] {
+    const hours: string[] = [];
+
+    for (let i = 8; i <= 24; i++) {
+      for (let j = 0; j <= 2; j++) {
+        const hour = i.toString().padStart(2, '0');
+        const minute = (j * 30).toString().padStart(2, '0');
+        hours.push(`${hour}:${minute}`);
+      }
+    }
+    return hours;
+  }
+
+  const hours = generateTimeSlots();
+
+  function toggleHour(hour: string) {
+    setSelectedHours((prev) =>
+      prev.includes(hour)
+        ? prev.filter((h) => h !== hour)
+        : [...prev, hour].sort(),
+    );
+  }
+
+  const timeZone = Intl.supportedValuesOf('timeZone').filter(
+    (zone) =>
+      zone.startsWith('America/Sao_Paulo') ||
+      zone.startsWith('America/Fortaleza') ||
+      zone.startsWith('America/Recife') ||
+      zone.startsWith('America/Bahia') ||
+      zone.startsWith('America/Belem') ||
+      zone.startsWith('America/Manaus') ||
+      zone.startsWith('America/Cuiaba') ||
+      zone.startsWith('America/Boa_Vista'),
+  );
+
+  async function onSubmit(values: ProfileFormData) {}
   return (
     <div className="mx-auto">
       <Form {...form}>
-        <form>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <Card>
             <CardHeader>
               <CardTitle>Meu Perfil</CardTitle>
@@ -61,6 +113,7 @@ export function ProfileContent() {
                           placeholder="Digite o nome da clinica"
                         />
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -76,6 +129,7 @@ export function ProfileContent() {
                       <FormControl>
                         <Input {...field} placeholder="Rua Martim afonso..." />
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -89,6 +143,7 @@ export function ProfileContent() {
                       <FormControl>
                         <Input {...field} placeholder="(41) 99556-xxxx" />
                       </FormControl>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -104,7 +159,7 @@ export function ProfileContent() {
                       <FormControl>
                         <Select
                           onValueChange={field.onChange}
-                          defaultValue={field.value ? "active" : "inactive"}
+                          defaultValue={field.value ? 'active' : 'inactive'}
                         >
                           <SelectTrigger>
                             <SelectValue placeholder="Selecione o status da clínica" />
@@ -123,6 +178,99 @@ export function ProfileContent() {
                   )}
                 />
               </div>
+
+              <div className="space-y-2">
+                <Label className="font-semibold">Configurar horários</Label>
+
+                <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+                  <DialogTrigger asChild>
+                    <Button
+                      className="w-full justify-between"
+                      variant={'outline'}
+                    >
+                      Clique aqui para selecionar horários{' '}
+                      <ArrowRight className="w-5 h-5" />
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Horários da clinica</DialogTitle>
+                      <DialogDescription>
+                        Selecione abaixo os horários de funcionamento da
+                        clinica:
+                      </DialogDescription>
+                    </DialogHeader>
+
+                    <section className="py-4">
+                      <p className="text-sm text-muted-foreground mb-2">
+                        Clique nos horários abaixo para marcar ou desmarcar:
+                      </p>
+
+                      <div className="grid grid-cols-5 gap-2">
+                        {hours.map((hour) => (
+                          <Button
+                            key={hour}
+                            variant={'outline'}
+                            className={cn(
+                              'h-10',
+                              selectedHours.includes(hour) &&
+                                'border-emerald-500 text-primary border-2',
+                            )}
+                            onClick={() => toggleHour(hour)}
+                          >
+                            {hour}
+                          </Button>
+                        ))}
+                      </div>
+                    </section>
+
+                    <Button
+                      className="w-full"
+                      onClick={() => setDialogIsOpen(false)}
+                    >
+                      Fechar modal
+                    </Button>
+                  </DialogContent>
+                </Dialog>
+              </div>
+
+              <FormField
+                control={form.control}
+                name="timeZone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold">
+                      Selecione o fuso horário
+                    </FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione o fuso horário" />
+                        </SelectTrigger>
+
+                        <SelectContent>
+                          {timeZone.map((zone) => (
+                            <SelectItem key={zone} value={zone}>
+                              {zone}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                className="w-full bg-emerald-500 hover:bg-emerald-400"
+              >
+                Salvar alterações
+              </Button>
             </CardContent>
           </Card>
         </form>
