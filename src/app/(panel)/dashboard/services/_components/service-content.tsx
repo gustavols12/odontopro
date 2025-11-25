81c9c0407389de8b8b55cf4f2c30ac5ff7665897
gustavo <gustavols2100@gmail.com>
2025-11-24 21:00:02 -0300

    feat: cria componente que exibe se plano ultrapassou o uso

--- src/app/(panel)/dashboard/services/_components/service-content.tsx
+++ src/app/(panel)/dashboard/services/_components/service-content.tsx
@@ -1,6 +1,7 @@
 import { canPermission } from "@/utilis/permissions/canPermission";
 import { getAllServices } from "../_data-access/get-all-services";
 import { ServicesList } from "./Services-list";
+import { LabelSubsciption } from "@/components/ui/label-subscription";
 
 interface ServicesContentProps {
   userId: string;
@@ -10,5 +11,12 @@ export async function ServicesContent({ userId }: ServicesContentProps) {
   const services = await getAllServices({ userId: userId });
   const permissions = await canPermission({ type: "service" });
 
-  return <ServicesList services={services.data || []} permissions={permissions} />;
+  return (
+    <>
+      {!permissions.hasPermission && (
+        <LabelSubsciption expired={permissions.expired} />
+      )}
+      <ServicesList services={services.data || []} permissions={permissions} />
+    </>
+  );
 }
