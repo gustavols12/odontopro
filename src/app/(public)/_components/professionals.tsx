e62b2ce79df74e47145ab111a367887b386e6c1e
gustavo <gustavols2100@gmail.com>
2025-11-27 22:03:05 -0300

    uptade: ajusta a home

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -11,10 +11,17 @@ import Image from "next/image";
 import image from "../../../../public/foto1.png";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import { User } from "@/generated/prisma";
+import { Prisma, User } from "@/generated/prisma";
+import { PremiumCardBadge } from "./PremiumCardBadge";
+
+type UserWithSubscription = Prisma.UserGetPayload<{
+  include: {
+    subscription: true;
+  };
+}>;
 
 interface ProfessionalsProps {
-  professionals: User[];
+  professionals: UserWithSubscription[];
 }
 
 export function Professionals({ professionals }: ProfessionalsProps) {
@@ -39,17 +46,20 @@ export function Professionals({ professionals }: ProfessionalsProps) {
                       fill
                       className="object-cover"
                     />
+
+                    {clinic.subscription?.status === "active" && (
+                      <PremiumCardBadge />
+                    )}
                   </div>
                 </div>
-                <div className="p-4 space-y-4">
+                <div className="p-4 space-y-4 min-h-[160px] flex flex-col justify-between">
                   <div className="flex items-center justify-between">
                     <div>
                       <h3 className="font-semibold">{clinic.name}</h3>
-                      <p className="text-sm text-gray-500">
+                      <p className="text-sm text-gray-500 line-clamp-2">
                         {clinic.address || "endereço não informado"}
                       </p>
                     </div>
-                    <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
                   </div>
                   <Link
                     href={`clinica/${clinic.id}`}
