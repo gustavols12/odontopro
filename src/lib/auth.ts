2db4acdfd32401f8a239d10380f31188bd114fa1
gustavo <gustavols2100@gmail.com>
2025-11-27 21:49:21 -0300

    Feat: cria login com o google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,12 @@
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import prisma from './prisma';
-import { Adapter } from 'next-auth/adapters';
-import GitHub from 'next-auth/providers/github';
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./prisma";
+import { Adapter } from "next-auth/adapters";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   trustHost: true,
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
